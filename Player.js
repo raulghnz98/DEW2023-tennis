147764efed921587abc9dd8b64335f393b5d4602
raulgarciahernandez005@gmail.com <raulgarciahernandez005@gmail.com>
2023-10-20 19:03:30 +0100

    Instalacion basica

---
+++ Player.js
@@ -0,0 +1,11 @@
+class Player {
+    name;
+
+    constructor(name){
+        this.name = name;
+    }
+}
+
+//Esto son pruebas (ELIMINAR A PARTIR DE AQUI)
+let player1 = new Player("Raúl");
+console.log(player1);
